2e2f028b4f6737454045ed39aa7a97dcfff10e18
Pavlova Marina <marizza.mil89@gmail.com>
2021-10-30 13:19:40 +0300

    02_Basics_08_Dynamic_Classes

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = Vue.createApp({
       return {
         showBooks: true,
         books: [
-            { title: 'Name of the wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg' },
-            { title: 'The way of kings', author: 'Brandon Sanderson', img: 'assets/2.jpg' },
-            { title: 'The final empire', author: 'Brandon Sanderson', img: 'assets/3.jpg' },
+            { title: 'Name of the wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg', isFav: true },
+            { title: 'The way of kings', author: 'Brandon Sanderson', img: 'assets/2.jpg', isFav: false },
+            { title: 'The final empire', author: 'Brandon Sanderson', img: 'assets/3.jpg', isFav: true },
         ],
       }
     },
